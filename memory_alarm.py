9de1698316c989c677b8fa5891a8d736755738a6
DySide <gks18092003@gmail.com>
2024-04-22 16:02:36 +0500

    First commit

--- memory_alarm.py
+++ memory_alarm.py
@@ -11,7 +11,7 @@ sleep_time = 20
 def main():
     while True:
         if psutil.virtual_memory().percent >= MEMORY_ALARM_PERCENT:
-            requests.post(API_URL, json={"message": "High memory usage detected!"})
+            requests.post(API_URL, json={"alert": "High memory usage detected!"})
         sleep(sleep_time)
 
 
